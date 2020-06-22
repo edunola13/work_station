6fdec7d12005a2d386f5234370bbfb758be027f2
Eduardo Nola <eduardo@bitpagos.co>
2020-06-22 00:48:10 -0300

    Fix docker-entrypoint

--- dockers/docker-entrypoint.sh
+++ dockers/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == "worker" ]]; then
     celery -A src.modules.celery worker -l info
 elif [[ "$1" == "beat" ]]; then
-    celery -A src.modules.celery beat -l info
+    celery -A src.modules.celery beat -l info --pidfile=
 else
     echo "Unknown Parameter"
 fi
